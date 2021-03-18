03ac4f2363c34af0ad5f56f63c7ed20567be6791
watherrock <watherrock@gmail.com>
2021-03-18 10:23:17 -0700

    ...

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
-<?php
+<html>
+	<head>
+		<title>Mi primer PHP</title>
+	</head>
+	<body>
+		<?php echo'<p>Hola mundo</p>'; 
+			mi_funcion(10,9);
+			echo"<p>hola</p>"
+		?>
+	</body>
 
-echo "Este mensaje se imprime con PHP";
 
-?>
+	<?php
+	function mi_funcion($a,$b){
+		$suma = $a + $b;
+		echo "El resultado es: " .$suma;
+	}
 
-<br />
-<p>Este mensaje se imprime con HTML</p><br />
+	?>
+	
+</html>
